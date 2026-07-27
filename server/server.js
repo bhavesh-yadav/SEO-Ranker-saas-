3774e99ca8cd6428216ee977238aa8bbe08791d5
Bhavesh Yadav <bhavesh2110@gmail.com>
2026-07-27 22:17:50 +0530

    DONE✅

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 import authRouter from "./routes/authRoutes.js";
 import rankRouter from "./routes/rankRoutes.js";
 import analysisRouter from "./routes/analysisRoutes.js";
+import { startRankTrackingCron } from "./cron/rankTrackingCron.js";
 
 console.log("BROWSERBASE_API_KEY:", process.env.BROWSERBASE_API_KEY);
 connectDB()
@@ -20,6 +21,9 @@ app.get('/',(req,res)=> res.send("Server is running"))
 app.use("/api/auth",authRouter)
 app.use("/api/rank",rankRouter)
 app.use('/api/analysis',analysisRouter)
+
+// Start cron job 
+startRankTrackingCron()
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
