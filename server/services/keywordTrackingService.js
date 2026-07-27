6e5935ca93baf09f615e7db2642c8c5e41794f6b
Bhavesh Yadav <bhavesh2110@gmail.com>
2026-07-28 00:45:57 +0530

    DONE✅

--- server/services/keywordTrackingService.js
+++ server/services/keywordTrackingService.js
@@ -1,4 +1,4 @@
-import { rankTracker } from "./rankTrackerservice.js";
+import { rankTracker } from "./rankTrackerService.js";
 
 export async function keywordTracking(tracking){
   console.log("🚀 keywordTracking STARTED");
